d49ecf90ba91d1f30b14eed4896ba9e13f249e98
mrkrcc <52096829+mrkrcc@users.noreply.github.com>
2021-02-04 20:01:15 +0300

    Update and rename dort_bir_mux to dort_bir_mux.vhd

--- 2x4_MUX/dort_bir_mux.vhd
+++ 2x4_MUX/dort_bir_mux.vhd
@@ -1,14 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity dort_bir_mux is
     Port ( I : in STD_LOGIC_VECTOR (3 downto 0);
